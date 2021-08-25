b26082a1b135c461a0b2ea85f19a63c42326f7eb
Alex Dean <adean@allgeier.ch>
2021-08-25 22:03:13 +0200

    added twilight sanct

--- Macros/Summon Drake.js
+++ Macros/Summon Drake.js
@@ -10,14 +10,14 @@ function selectShape(drakeactorname){
                         <h2>Select your Type</h2>
                         <div style="flex:1">Type:<select name="drake">${typeDialogOptions}</select></div>
                         `;
-new Dialog({
-        title: "Drake Companion Type",
-        content: dialogContent,
-        buttons: {
-            accept_button: {
-                label: `<i class="fas fa-dragon"></i>Summon Drake!`,
-                callback: async (html) => {                    
-                    let draketype = html.find("[name=drake]")[0].value;
+	new Dialog({
+		title: "Drake Companion Type",
+		content: dialogContent,
+		buttons: {
+			accept_button: {
+				label: `<i class="fas fa-dragon"></i>Summon Drake!`,
+				callback: async (html) => {                    
+					let draketype = html.find("[name=drake]")[0].value;
 					let damagetype = draketype.toLowerCase();
 					let creature = game.actors.getName(drakeactorname);
 					const effect_name = draketype + " Type";
@@ -53,29 +53,29 @@ new Dialog({
 					}					
 					drake = await spawnActor(canvas.scene,canvas.templates.placeables.map(x=>x).reverse().find(t => t.data.user == game.user.id)?.data || canvas.tokens.get(token.data._id),draketype);
 					
-                }
-            }
-        },
-        default: "accept_button"
-    }).render(true);
+				}
+			}
+		},
+		default: "accept_button"
+	}).render(true);
 }
+
+function spawnActor(scene, template,draketype) {
+	
+	let protoToken = duplicate(game.actors.getName(drakeactorname).data.token);
+	protoToken.x = template.x;
+	protoToken.name = draketype + " Drake";
+	protoToken.y = template.y;
+	protoToken.flags.summoner = token.data._id;
+	// Increase this offset for larger summons
+	protoToken.x -= (scene.data.grid/2+(protoToken.width-1)*scene.data.grid);
+	protoToken.y -= (scene.data.grid/2+(protoToken.height-1)*scene.data.grid);		 
+	return canvas.scene.createEmbeddedDocuments("Token",[protoToken]);
+}
+
 let drakeactorname = "Drake Companion";
-//debugger
-debugger
+// use this to acticate debugging in the brwoser. useful to see what's currently happening with the code. Needs Developer Console to be on. i.e. F12
+//debugger 
 selectShape(drakeactorname)
 /** intended for use with Item Macro. 'item' here is the spell being cast if using outside Item Macro */
 /** spawns an actor with the same name as the spell at the location of the template */
-//(async ()=>{
-    function spawnActor(scene, template,draketype) {
-       
-         let protoToken = duplicate(game.actors.getName(drakeactorname).data.token);
-         protoToken.x = template.x;
-		 protoToken.name = draketype + " Drake";
-         protoToken.y = template.y;
-         protoToken.flags.summoner = token.data._id;
-         // Increase this offset for larger summons
-         protoToken.x -= (scene.data.grid/2+(protoToken.width-1)*scene.data.grid);
-         protoToken.y -= (scene.data.grid/2+(protoToken.height-1)*scene.data.grid);		 
-         return canvas.scene.createEmbeddedDocuments("Token",[protoToken]);
-     }
-//})();
