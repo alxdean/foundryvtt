0e22c5092f9cad9a41acff26b9ae15d8a661b4e4
Alex Dean <adean@allgeier.ch>
2021-08-29 21:02:35 +0200

    fixed targeting of award goodberries to use the player target and not GM target

--- Macros/Award Goodberry.js
+++ Macros/Award Goodberry.js
@@ -1,17 +1,19 @@
 //Award Goodberry Macro. Make sure to set this up as "Award Goodberry" in the Macro Library and is set to run as GM
+let player = args[0].actor;
+let targets=args[0].hitTargets;
 let target;
-if(game.user.targets.size > 1) return ui.notifications.error(`Please target exactly 1 token.`);
-if(game.user.targets.size == 1) 
-	target = game.user.targets.values().next().value
+if(targets.length > 1) return ui.notifications.error(`Please target exactly 1 token.`);
+if(targets.length == 1) 
+	target = game.actors.get(targets[0].actorId);
 else
-	target = canvas.tokens.controlled[0]
-if(target.actor.data.data.attributes.hp.value<target.actor.data.data.attributes.hp.max)
+	target = player;
+if(target.data.data.attributes.hp.value<target.data.data.attributes.hp.max)
 {
-	let hp = Math.clamped(target.actor.data.data.attributes.hp.value + 1, 0, target.actor.data.data.attributes.hp.max);
-	target.actor.data.data.attributes.hp.value = hp;
-    	await ChatMessage.create({content: `${target.actor.name} gains 1 HP`, speaker:{alias: canvas.tokens.controlled[0].actor.name}, type: CONST.CHAT_MESSAGE_TYPES.OOC});
+	let hp = target.data.data.attributes.hp.value + 1;
+	target.data.data.attributes.hp.value = hp;
+    	await ChatMessage.create({content: `${target.name} gains 1 HP`, speaker:{alias: player.name}, type: CONST.CHAT_MESSAGE_TYPES.OOC});
 }
 else
 {
-	await ChatMessage.create({content: `${target.actor.name} gains no HP as they are at max health. They feel rather full`, speaker:{alias: canvas.tokens.controlled[0].actor.name}, type: CONST.CHAT_MESSAGE_TYPES.OOC});
+	await ChatMessage.create({content: `${target.name} gains no HP as they are at max health. They feel rather full`, speaker:{alias: player.name}, type: CONST.CHAT_MESSAGE_TYPES.OOC});
 }
