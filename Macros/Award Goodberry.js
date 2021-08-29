9a60c06e25d7296ddde2824566189bed6829d382
Alex Dean <adean@allgeier.ch>
2021-08-29 21:26:51 +0200

    fixed targeting of too many players, and also when player has no targets

--- Macros/Award Goodberry.js
+++ Macros/Award Goodberry.js
@@ -1,12 +1,20 @@
 //Award Goodberry Macro. Make sure to set this up as "Award Goodberry" in the Macro Library and is set to run as GM
 let player = args[0].actor;
-let targets=args[0].hitTargets;
+let targets=args[0].targets;
 let target;
-if(targets.length > 1) return ui.notifications.error(`Please target exactly 1 token.`);
-if(targets.length == 1) 
+if(targets.length > 1) {
 	target = game.actors.get(targets[0].actorId);
+	ChatMessage.create({content: `Too many targets! only one target will receive a good berry.`, speaker:{alias: player.name}, type: CONST.CHAT_MESSAGE_TYPES.OOC});	
+}
 else
-	target = player;
+{
+	if(targets.length == 1) 
+		target = game.actors.get(targets[0].actorId);
+	else
+		target = game.actors.get(speaker.actor);;
+}
+
+
 if(target.data.data.attributes.hp.value<target.data.data.attributes.hp.max)
 {
 	let hp = target.data.data.attributes.hp.value + 1;
