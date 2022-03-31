158768a8bfa227c1b477e38a32c49792d61358fe
Alex Dean <adean@allgeier.ch>
2022-04-01 00:14:39 +0200

    updated summon macros to use templates for placing

--- Macros/Summon Drake.js
+++ Macros/Summon Drake.js
@@ -51,7 +51,89 @@ function selectShape(drakeactorname){
 								await creature.updateEmbeddedDocuments("ActiveEffect", [{"_id": effect_id, "disabled" : false}]);			
 						}				
 					}					
-					drake = await spawnActor(canvas.scene,canvas.templates.placeables.map(x=>x).reverse().find(t => t.data.user == game.user.id)?.data || canvas.tokens.get(token.data._id),draketype);
+					if (!game.modules.get("advanced-macros")?.active) ui.notifications.error("Please enable the Advanced Macros module")
+
+					const lastArg = args[args.length - 1];
+					let tactor;
+					if (lastArg.tokenId) tactor = canvas.tokens.get(lastArg.tokenId).actor;
+					else tactor = game.actors.get(lastArg.actorId);
+					const target = canvas.tokens.get(lastArg.tokenId) || token;
+					
+					
+					
+						let range = canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [{
+							t: "circle",
+							user: game.user._id,
+							x: target.x + canvas.grid.size / 2,
+							y: target.y + canvas.grid.size / 2,
+							direction: 0,
+							distance: 30,
+							borderColor: "#FF0000",
+							flags: {
+								DAESRD: {
+									Summoner: {
+										ActorId: tactor.id
+									}
+								}
+							}
+							//fillColor: "#FF3366",
+						}]);
+					
+						range.then(result => {
+							let templateData = {
+								t: "rect",
+								user: game.user._id,
+								distance: 7.5,
+								direction: 45,
+								x: 0,
+								y: 0,
+								fillColor: game.user.color,
+								flags: {
+									DAESRD: {
+										Summoner: {
+											ActorId: tactor.id
+										}
+									}
+								}
+							};
+					
+					
+					
+							Hooks.once("createMeasuredTemplate", deleteTemplatesAndSpawn);
+							let doc = new CONFIG.MeasuredTemplate.documentClass(templateData, { parent: canvas.scene })
+							let template = new game.dnd5e.canvas.AbilityTemplate(doc);
+							template.actorSheet = tactor.sheet;
+							template.drawPreview();
+					
+							async function deleteTemplatesAndSpawn(template) {
+								//debugger
+								let removeTemplates = canvas.templates.placeables.filter(i => i.data.flags.DAESRD?.Summoner?.ActorId === tactor.id);
+								let templateArray = removeTemplates.map(function (w) {
+									return w.id
+								})  
+								//debugger
+								var drake = await spawnActor(canvas.scene,template,draketype);
+								
+								//await target.document.update({ x: template.data.x, y: template.data.y } , {animate : false})
+								if (removeTemplates) await canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", templateArray)
+								//await tactor.deleteEmbeddedDocuments("ActiveEffect", [lastArg.effectId]); 
+							};
+						});
+						
+						function spawnActor(scene, template,draketype) {	
+							let protoToken = duplicate(game.actors.getName(drakeactorname).data.token);
+							protoToken.x = template.data.x;
+							protoToken.name = draketype + " Drake";
+							protoToken.y = template.data.y;
+							protoToken.flags.summoner = token.data._id;
+							// Increase this offset for larger summons
+							protoToken.x -= (scene.data.grid/2+(protoToken.width-1)*scene.data.grid);
+							protoToken.y -= (scene.data.grid/2+(protoToken.height-1)*scene.data.grid);		 
+							return canvas.scene.createEmbeddedDocuments("Token",[protoToken]);
+						}
+						
+						
+																
 					
 				}
 			}
@@ -60,22 +142,11 @@ function selectShape(drakeactorname){
 	}).render(true);
 }
 
-function spawnActor(scene, template,draketype) {
-	
-	let protoToken = duplicate(game.actors.getName(drakeactorname).data.token);
-	protoToken.x = template.x;
-	protoToken.name = draketype + " Drake";
-	protoToken.y = template.y;
-	protoToken.flags.summoner = token.data._id;
-	// Increase this offset for larger summons
-	protoToken.x -= (scene.data.grid/2+(protoToken.width-1)*scene.data.grid);
-	protoToken.y -= (scene.data.grid/2+(protoToken.height-1)*scene.data.grid);		 
-	return canvas.scene.createEmbeddedDocuments("Token",[protoToken]);
-}
-
 let drakeactorname = "Drake Companion";
 // use this to acticate debugging in the brwoser. useful to see what's currently happening with the code. Needs Developer Console to be on. i.e. F12
 //debugger 
+
+
 selectShape(drakeactorname)
 /** intended for use with Item Macro. 'item' here is the spell being cast if using outside Item Macro */
 /** spawns an actor with the same name as the spell at the location of the template */
--- Macros/SummonFamiliar.js
+++ Macros/SummonFamiliar.js
@@ -1,21 +1,89 @@
+//DAE Macro Execute, Effect Value = "Macro Name" @target 
+if (!game.modules.get("advanced-macros")?.active) ui.notifications.error("Please enable the Advanced Macros module")
 
-function spawnActor(scene, template,spawnName) {
+const lastArg = args[args.length - 1];
+let tactor;
+if (lastArg.tokenId) tactor = canvas.tokens.get(lastArg.tokenId).actor;
+else tactor = game.actors.get(lastArg.actorId);
+const target = canvas.tokens.get(lastArg.tokenId) || token;
+
+
+
+    let range = canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [{
+        t: "circle",
+        user: game.user._id,
+        x: target.x + canvas.grid.size / 2,
+        y: target.y + canvas.grid.size / 2,
+        direction: 0,
+        distance: 30,
+        borderColor: "#FF0000",
+        flags: {
+            DAESRD: {
+                Summoner: {
+                    ActorId: tactor.id
+                }
+            }
+        }
+        //fillColor: "#FF3366",
+    }]);
+
+    range.then(result => {
+        let templateData = {
+            t: "rect",
+            user: game.user._id,
+            distance: 7.5,
+            direction: 45,
+            x: 0,
+            y: 0,
+            fillColor: game.user.color,
+            flags: {
+                DAESRD: {
+                    Summoner: {
+                        ActorId: tactor.id
+                    }
+                }
+            }
+        };
+
+
+
+        Hooks.once("createMeasuredTemplate", deleteTemplatesAndSpawn);
+        let doc = new CONFIG.MeasuredTemplate.documentClass(templateData, { parent: canvas.scene })
+        let template = new game.dnd5e.canvas.AbilityTemplate(doc);
+        template.actorSheet = tactor.sheet;
+        template.drawPreview();
+
+        async function deleteTemplatesAndSpawn(template) {
+            //debugger
+            let removeTemplates = canvas.templates.placeables.filter(i => i.data.flags.DAESRD?.Summoner?.ActorId === tactor.id);
+            let templateArray = removeTemplates.map(function (w) {
+                return w.id
+            })  
+            //debugger
+            await spawnActor(canvas.scene,template,"Owl Familiar");     
+            //await target.document.update({ x: template.data.x, y: template.data.y } , {animate : false})
+            if (removeTemplates) await canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", templateArray)
+            //await tactor.deleteEmbeddedDocuments("ActiveEffect", [lastArg.effectId]); 
+        };
+    });
+    
+    function spawnActor(scene, template,spawnName) {
 	
-	let protoToken = duplicate(game.actors.getName(spawnName).data.token);
-	protoToken.x = template.x;
-	protoToken.name = spawnName;
-	protoToken.y = template.y;
-	protoToken.flags.summoner = token.data._id;
-	// Increase this offset for larger summons
-	protoToken.x -= (scene.data.grid/2+(protoToken.width-1)*scene.data.grid);
-	protoToken.y -= (scene.data.grid/2+(protoToken.height-1)*scene.data.grid);		 
-	return canvas.scene.createEmbeddedDocuments("Token",[protoToken]);
-}
-
-let spawnName = "Owl Familiar";
-spawn = await spawnActor(canvas.scene,canvas.templates.placeables.map(x=>x).reverse().find(t => t.data.user == game.user.id)?.data || canvas.tokens.get(token.data._id),spawnName);
-// use this to acticate debugging in the brwoser. useful to see what's currently happening with the code. Needs Developer Console to be on. i.e. F12
-//debugger 
-
-/** intended for use with Item Macro. 'item' here is the spell being cast if using outside Item Macro */
-/** spawns an actor with the same name as the spell at the location of the template */
+        let protoToken = duplicate(game.actors.getName(spawnName).data.token);
+        protoToken.x = template.data.x;
+        protoToken.name = spawnName;
+        protoToken.y = template.data.y;
+        protoToken.flags.summoner = token.data._id;
+        // Increase this offset for larger summons
+        protoToken.x -= (scene.data.grid/2+(protoToken.width-1)*scene.data.grid);
+        protoToken.y -= (scene.data.grid/2+(protoToken.height-1)*scene.data.grid);		 
+        return canvas.scene.createEmbeddedDocuments("Token",[protoToken]);
+    }
+    
+    //let spawnName = "Owl Familiar";
+    //spawn = await spawnActor(canvas.scene,canvas.templates.placeables.map(x=>x).reverse().find(t => t.data.user == game.user.id)?.data || canvas.tokens.get(token.data._id),spawnName);
+    // use this to acticate debugging in the brwoser. useful to see what's currently happening with the code. Needs Developer Console to be on. i.e. F12
+    //debugger 
+    
+    /** intended for use with Item Macro. 'item' here is the spell being cast if using outside Item Macro */
+    /** spawns an actor with the same name as the spell at the location of the template */
