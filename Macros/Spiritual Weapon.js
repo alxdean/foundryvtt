7c2d2dfbb7060d1c8881669444aa2ef2bca4b114
Alex Dean <adean@allgeier.ch>
2022-04-01 15:00:19 +0200

    new version of Spiritual Weapon

--- Macros/Spiritual Weapon.js
+++ Macros/Spiritual Weapon.js
@@ -1,69 +1,118 @@
-//DAE Item Macro Execute, value = @item.level
-// Set spell to self cast, no damage/attack roll
+//DAE Macro Execute, Effect Value = "Macro Name" @target 
+if (!game.modules.get("advanced-macros")?.active) ui.notifications.error("Please enable the Advanced Macros module")
+
 const lastArg = args[args.length - 1];
 let tactor;
 if (lastArg.tokenId) tactor = canvas.tokens.get(lastArg.tokenId).actor;
 else tactor = game.actors.get(lastArg.actorId);
 const target = canvas.tokens.get(lastArg.tokenId) || token;
+debugger
+let mod = tactor.data.data.abilities[args[2]].mod
 
-const castingItem = lastArg.efData.flags.dae.itemData
 let data = {}
-
+//debugger
 /**
  * Create Spiritual Weapon item in inventory
  */
 if (args[0] === "on") {
-  let damage = Math.floor(Math.floor(args[1] / 2));
-  let image = castingItem.img;
-
-  let range = canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [{
-    t: "circle",
-    user: game.user._id,
-    x: target.x + canvas.grid.size / 2,
-    y: target.y + canvas.grid.size / 2,
-    direction: 0,
-    distance: 60,
-    borderColor: "#FF0000",
-    flags: {
-      DAESRD: {
-        SpiritualWeaponRange: {
-          ActorId: tactor.id
+    let range = canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [{
+        t: "circle",
+        user: game.user._id,
+        x: target.x + canvas.grid.size / 2,
+        y: target.y + canvas.grid.size / 2,
+        direction: 0,
+        distance: 60,
+        borderColor: "#FF0000",
+        flags: {
+            DAESRD: {
+                Summoner: {
+                    ActorId: tactor.id
+                }
+            }
         }
-      }
-    }
-    //fillColor: "#FF3366",
-  }]);
-  range.then(result => {
-    let templateData = {
-      t: "rect",
-      user: game.user._id,
-      distance: 7,
-      direction: 45,
-	texture: "systems/dnd5e/icons/items/weapons/warhammer.png",
-      x: 0,
-      y: 0,
-      flags: {
-        DAESRD: {
-          SpiritualWeapon: {
-            ActorId: tactor.id
-          }
-        }
-      },
-      fillColor: game.user.color
-    }
-    Hooks.once("createMeasuredTemplate", deleteTemplates);
+        //fillColor: "#FF3366",
+    }]);
 
-    let doc = new CONFIG.MeasuredTemplate.documentClass(templateData, { parent: canvas.scene })
-    let template = new game.dnd5e.canvas.AbilityTemplate(doc)
-    template.actorSheet = tactor.sheet;
-    template.drawPreview()
+    range.then(result => {
+        let templateData = {
+            t: "rect",
+            user: game.user._id,
+            distance: 7.5,
+            direction: 45,
+            x: 0,
+            y: 0,
+            fillColor: game.user.color,
+            flags: {
+                DAESRD: {
+                    Summoner: {
+                        ActorId: tactor.id
+                    }
+                }
+            }
+        };
 
-    async function deleteTemplates() {
-      let removeTemplates = canvas.templates.placeables.filter(i => i.data.flags.DAESRD?.SpiritualWeaponRange?.ActorId === tactor.id);
-      await canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [removeTemplates[0].id]);
-    };
-  })
-  await tactor.createOwnedItem(
+
+
+        Hooks.once("createMeasuredTemplate", deleteTemplatesAndSpawn);
+        let doc = new CONFIG.MeasuredTemplate.documentClass(templateData, { parent: canvas.scene })
+        let template = new game.dnd5e.canvas.AbilityTemplate(doc);
+        template.actorSheet = tactor.sheet;
+        template.drawPreview();
+
+        async function deleteTemplatesAndSpawn(template) {
+            //debugger
+            let removeTemplates = canvas.templates.placeables.filter(i => i.data.flags.DAESRD?.Summoner?.ActorId === tactor.id);
+            let templateArray = removeTemplates.map(function (w) {
+                return w.id
+            })  
+            //debugger
+            let damage = Math.floor(Math.floor(args[1] / 2));            
+            var weapons = await spawnActor(canvas.scene,template,"Spiritual Weapon");     
+            if (removeTemplates) await canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", templateArray)
+
+            //debugger
+
+            let weapon = weapons[0].actor            
+            let strike = weapon.items.find(item => item.data.name == "Warhammer");
+            if (strike == null || strike == undefined)
+            {
+              ui.notification.error("Weapon not found");
+              return;
+            }
+            await strike.update({
+              "data.damage.parts": [
+                [
+                `${damage}d8+${mod}`,
+                `force`
+                ]
+              ]}															
+            );
+  
+           
+
+        };
+    });
+    
+    function spawnActor(scene, template,spawnName) {
+	
+        let protoToken = duplicate(game.actors.getName(spawnName).data.token);
+        protoToken.x = template.data.x;
+        protoToken.name = spawnName;
+        protoToken.y = template.data.y;
+        protoToken.flags.SpiritualWeapon = tactor.id;        
+        // Increase this offset for larger summons
+        protoToken.x -= (scene.data.grid/2+(protoToken.width-1)*scene.data.grid);
+        protoToken.y -= (scene.data.grid/2+(protoToken.height-1)*scene.data.grid);		 
+        return canvas.scene.createEmbeddedDocuments("Token",[protoToken]);
+    }
+}
+if (args[0] === "off") {
+  
+  let weapons = canvas.tokens.ownedTokens.filter(i => i.data.flags.SpiritualWeapon === tactor.id)
+if (weapons.length>0)
+  await canvas.scene.deleteEmbeddedDocuments("Token", [weapons[0].id])
+}
+  /* await tactor.createEmbeddedDocuments("Item", [
     {
       "name": "Summoned Spiritual Weapon",
       "type": "weapon",
@@ -107,15 +156,7 @@ if (args[0] === "on") {
       "img": `${image}`,
       "effects" : []
     },
-  );
+  ]);
   ui.notifications.notify("Weapon created in your inventory")
 
-}
-
-// Delete Spitirual Weapon and template
-if (args[0] === "off") {
-  let removeItem = tactor.items.find(i => i.data.flags?.DAESRD?.SpiritualWeapon === tactor.id)
-  let template = canvas.templates.placeables.find(i => i.data.flags.DAESRD.SpiritualWeapon?.ActorId === tactor.id)
-  if(removeItem) await tactor.deleteOwnedItem(removeItem.id);
-  if(template) await canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [template.id])
-}
+} */
